25144824a64fbd5acde6f3e2c1d04b3dd2f80700
Erick Saavedra <esvdrah@gmail.com>
2020-07-20 14:38:18 -0500

    form config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
   return (
     <Fragment>
       <Header title='Open Weather' />
+
+      <div className='contenedor-form'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col m6 s12'>
+              <Form />
+            </div>
+            <div className='col m6 s12'>2</div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
